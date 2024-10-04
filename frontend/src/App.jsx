285d5434bf4e37b5d0b2ae46d344cff2ad2c7a27
Jayant Kushwah <jayantkushwah8@gmail.com>
2024-10-04 16:43:38 +0530

    added bestseller newsletter ourPolicy in homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,35 @@
-import { Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import Cart from "./pages/Cart"
-import Collections from "./pages/Collections"
-import Login from "./pages/Login"
-import About from "./pages/About"
-import Contact from "./pages/Contact"
-import Product from "./pages/Product"
-import PlaceOrder from "./pages/PlaceOrder"
-import Orders from "./pages/Orders"
-import Navbar from "./components/Navbar"
-
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Collections from "./pages/Collections";
+import Login from "./pages/Login";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Product from "./pages/Product";
+import PlaceOrder from "./pages/PlaceOrder";
+import Orders from "./pages/Orders";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
-
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
-    <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/orders" element={<Orders/>} />
-        <Route path="/place-orders" element={<PlaceOrder/>} />
-        <Route path="/" element={<Home/>} />
-        <Route path="/cart" element={<Cart/>} />
-        <Route path="/collection" element={<Collections/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/contact" element={<Contact/>} />
-        <Route path="/product/:productId" element={<Product/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/place-orders" element={<PlaceOrder />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/collection" element={<Collections />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product/:productId" element={<Product />} />
       </Routes>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { ShopContextProvider , ShopContext } from "./context/ShopContext.jsx";
+export { ShopContext};
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+    <ShopContextProvider>
+      <App />
+    </ShopContextProvider>
+  </BrowserRouter>
+);
