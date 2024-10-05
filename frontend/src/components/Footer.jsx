c27217259a65332c66922c36a807c8ae5619c4b8
Jayant Kushwah <jayantkushwah8@gmail.com>
2024-10-05 22:07:34 +0530

    footer, collecion page ,category

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,13 +1,43 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../assets/assets";
 
 const Footer = () => {
   return (
     <div>
-      <div>
-        
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm ">
+        <div>
+          <img src={assets.logo} className="mb-5 w-32 " alt="" />
+          <p className="w-full md:w-2/3 text-gray-600 ">
+            © 2024 Forever. All rights reserved.
+            <br /> Secure payments, fast shipping, and 24/7 customer support for
+            a seamless shopping experience.
+          </p>
+        </div>
+        <div>
+          <p className="text-xl font-medium mb-5 ">COMPANY</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div>
+          <p className="text-xl font-medium mb-5 ">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>+91 87704-43828</li>
+            <li>jayantkushwah24@gmail.com</li>
+          </ul>
+        </div>
+        {/* <div>
+          <hr />
+          <p className="py-5 text-sm text-center ">
+            © 2024 Forever. All right reserved
+          </p>
+        </div> */}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,17 @@
-import  { useState } from "react";
+import { useContext, useState } from "react";
 import { assets } from "../assets/assets";
 import { Link, NavLink } from "react-router-dom";
+import { ShopContext } from "../main";
 
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
+  const { setShowSearch } = useContext(ShopContext);
 
   return (
     <div className="flex items-center justify-between py-5 font-medium">
-      <Link to='/'><img src={assets.logo} className="w-36" /></Link>
+      <Link to="/">
+        <img src={assets.logo} className="w-36" />
+      </Link>
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
         <NavLink to="/" className="flex flex-col items-center gap-1">
           <p>HOME</p>
@@ -27,7 +31,11 @@ const Navbar = () => {
         </NavLink>
       </ul>
       <div className="flex items-center gap-6 ">
-        <img src={assets.search_icon} className="w-5 cursor-pointer" />
+        <img
+          onClick={() => setShowSearch(true)}
+          src={assets.search_icon}
+          className="w-5 cursor-pointer"
+        />
         <div className="group relative">
           <img src={assets.profile_icon} className="w-5 cursor-pointer" />
           <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
